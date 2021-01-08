5aa1c104f4d00840b0ba446afdc00006f532a430
Rafael Reyes <r.reyes@caribemedia.com.do>
2021-01-08 10:08:20 -0400

    Starting project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoComponent } from './components/todos/todo.component';
+import { TodoItemComponent } from './components/todos/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
