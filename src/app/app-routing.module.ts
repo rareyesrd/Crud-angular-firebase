168515c4c8f2679cb18ae5fac59afed1f534625c
Rafael Reyes <rafaelalexanderr5@gmail.com>
2021-01-10 20:37:24 -0400

    UPDATE - routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { HeaderComponent } from './components/header/header.component';
 import { TodoComponent } from './components/todos/todo.component';
 
 const routes: Routes = [
